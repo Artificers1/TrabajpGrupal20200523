7555f46826be81fc849678488111b5c3fc100afa
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-05-21 15:35:54 -0600

    Pag1E1 - Se creo la clase Animal junto sus atributos y constructores

--- TrabajoGrupal/src/App.java
+++ TrabajoGrupal/src/App.java
@@ -2,4 +2,23 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
+
+    static class Animal {
+        //Propiedades privadas
+        private String nombre;
+        private String orden;
+        private int extremidades;
+    
+        //constructor vacío para inicializar
+        public Animal() {
+        }
+    
+        //constructor con parámetros
+        public Animal(String nombre, String orden, int extremidades) {
+            this.nombre = nombre;
+            this.orden = orden;
+            this.extremidades = extremidades;
+        }
+    
+    }
 }
