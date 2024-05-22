c6e26c9782292a7be306ad9f54121a68d3fff0f2
Emerson1911 <1284662@clases.edu.sv>
2024-05-21 21:11:31 -0600

    Terminacion de ejercicio

--- TrabajoGrupal/src/EjercicioPagina2/App.java
+++ TrabajoGrupal/src/EjercicioPagina2/App.java
@@ -1,5 +1,15 @@
 package EjercicioPagina2;
 
 public class App {
-    
+    public static void main(String[] args) throws Exception {
+        // Creación de un objeto Empleado
+        Empleado empleado = new Empleado("Juan Pérez", 1000.0, 10, 15.0);
+
+        // Registro del empleado
+        empleado.registrar();
+
+        // Cálculo del sueldo del empleado
+        double sueldo = empleado.calcularSueldo();
+        System.out.println("El sueldo total de " + empleado.getNombre() + " es: " + sueldo);
+    }
 }
