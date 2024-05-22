296d336c7c34e52dfb920876db540c6d06cf183d
Ángel Miguel Mena Argumedo <angelmeba4@gmail.com>
2024-05-21 18:30:23 -0600

    ejercicio 1 pag 1

--- TrabajoGrupal/src/App.java
+++ TrabajoGrupal/src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Animal perro = new Animal("Dinosaurio", "Carnivoro", 4);
+
+        System.out.println("Su nombre es: "+perro.getNombre());
+        System.out.println("Su Orden es: "+perro.getOrden());
+        System.out.println("Sus Extremidades son: "+perro.getExtremidades());
+        
     }
 
 }
