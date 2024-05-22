4b43445803df26af078719d31c3eb5ff8851f605
Rolando Stiven Flamenco Guerra <redloreheart@gmail.com>
2024-05-21 21:05:49 -0600

    cy

--- TrabajoGrupal/src/EjercicioPagina2/Empleado.java
+++ TrabajoGrupal/src/EjercicioPagina2/Empleado.java
@@ -1,5 +1,16 @@
 package EjercicioPagina2;
 
 public class Empleado extends Persona {
+    private String nombre;
+    private double salarioBase;
+    private int horasExtras;
+    private double pagoPorHoraExtra;
+
+    public Empleado(String nombre, double salarioBase, int horasExtras, double pagoPorHoraExtra) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasExtras = horasExtras;
+        this.pagoPorHoraExtra = pagoPorHoraExtra;
+    }
     
 }
